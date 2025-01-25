7150a5a8d765d80d47863e5f379eaefb4d7eb505
Federico Gerardi <federico.gerardi03@gmail.com>
2025-01-25 10:53:07 +0100

    Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ViewTransitions } from 'next-view-transitions';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -33,6 +34,7 @@ export default function RootLayout({
             </main>
             <Footer />
             <Analytics />
+            <SpeedInsights />
           </div>
         </body>
       </html>
